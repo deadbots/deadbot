c80f8e697a6ed5e17924b6969daff7a30988da8e
Crackexy <63198834+Crackexy@users.noreply.github.com>
2020-05-31 14:26:52 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -13,7 +13,7 @@ PREV_REPLY_MESSAGE = {}
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No name set yet nibba, check pinned message in** @TeleBotHelp"
 USER_BOT_WARN_ZERO = "`You were spamming my master's inbox, henceforth your retarded lame ass has been blocked by my master's userbot.` **Now GTFO, i'm playing minecraft** "
 USER_BOT_NO_WARN = ("[...........███ ]▄▄▄▄▄▃ \n..▂▄▅█████▅▄▃▂\n[███████████████]\n◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤](tg://user?id=719195224)\n\n"
-                    "`Hello, this is DEADBOY'S BODYGUARD. This is an automated message in `"
+                    "`Hello, this is DEADBOY'S BodyGuard. This is an automated message in `"
                     f"{DEFAULTUSER}'s` inbox.\n\n"
                     "Do not spam here, else you will be blocked automatically.`\n\n"
                     "** Send** `/start` ** to access the available options.**")

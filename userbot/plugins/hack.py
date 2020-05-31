a243c6d681ac7b12cbfb18c8cab09a7c8203ddaf
Crackexy <63198834+Crackexy@users.noreply.github.com>
2020-05-31 14:03:15 +0530

    Update hack.py

--- userbot/plugins/hack.py
+++ userbot/plugins/hack.py
@@ -43,7 +43,7 @@ async def _(event):
             "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
             "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
             "`Hacking... 100%\n█████████HACKED███████████ `",
-            "`Targeted Account Hacked...\n\nPay 69$ To` @TeleBotHelp `Or send nudes of female Homo Sapiens To Remove This Hack`"
+            "`Targeted Account Hacked...\n\nPAY 1 CRORE TO DEAD BOY OR PRAISE DEAD BOY TO REMOVE THIS HACK"
         ]
 
         for i in animation_ttl:

f1c3fc7471b3e6cec1e3e89577932f530d38517a
deadlyxd <66526325+deadlyxd@users.noreply.github.com>
2020-06-18 14:00:25 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,12 +8,12 @@ from userbot.utils import admin_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet, check pinned in @TeleBotHelp"
 
-@command(outgoing=True, pattern="^.alive$")
+@command(outgoing=True, pattern="^.al$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("   **𝙳𝙴𝙰𝙳𝙱𝙾𝚃 𝙾𝙵𝙵𝙸𝙲𝙸𝙰𝙻 ** \n\n"
         "**`𝙻𝚘𝚕! 𝚉𝚒𝚗𝚍𝚊 𝙷𝚞𝚗 𝙱𝚑𝚊𝚒. 𝙰𝚕𝚕 𝚜𝚢𝚜𝚝𝚎𝚖𝚜 𝚘𝚗𝚕𝚒𝚗𝚎 𝚊𝚗𝚍 𝚏𝚞𝚗𝚌𝚝𝚒𝚘𝚗𝚒𝚗𝚐 𝚗𝚘𝚛𝚖𝚊𝚕𝚕𝚢... ψ(^_^)ψ`**\n\n"
-                     "` 🔸 𝚃𝚎𝚕𝚎𝚝𝚑𝚘𝚗 𝚟𝚎𝚛𝚜𝚒𝚘𝚗:` **6.9.0**\n  🔹 𝙿𝚢𝚝𝚑𝚘𝚗:` **3.7.3** \n`"
+                     "` 🔸 𝚃𝚎𝚕𝚎𝚝𝚑𝚘𝚗 𝚟𝚎𝚛𝚜𝚒𝚘𝚗:` **6.9.0**\n  🔹  𝙿𝚢𝚝𝚑𝚘𝚗:` **3.7.3** \n`"
                      "` 🔹 𝙱𝚘𝚝 𝚌𝚛𝚎𝚊𝚝𝚎𝚍 𝚋𝚢:` [DEAD BOY](tg://user?id=1247419158)\n"
                      "` 🔸 𝙳𝚊𝚝𝚊𝚋𝚊𝚜𝚎 𝚂𝚝𝚊𝚝𝚞𝚜:` **All OK 👌!**\n"
                      "` 🔹 𝙼𝚢 𝙻𝚘𝚗𝚎𝚕𝚢 𝚘𝚠𝚗𝚎𝚛:` DEAD BOY")

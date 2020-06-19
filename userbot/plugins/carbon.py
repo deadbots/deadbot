b767f748cedc45ae84dc2d92f14a50121e854832
deadlyxd <66526325+deadlyxd@users.noreply.github.com>
2020-06-19 18:58:35 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -99,11 +99,11 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="<< `Here's your carbon!` \n **Carbonised by Dead Boy** [@TeleBotHelp.](https://g>>\n**Colour Scheme: **`{}`".format(color_name),
+         caption="<< `Here's your carbon!` \n **Carbonised by Shreya**\n**Colour Scheme: **`{}`".format(color_name),
          force_document=True,
          reply_to=e.message.reply_to_msg_id,
          )
-   os.remove('./Anubis.png')
+   os.remove('./deadboy.png')
    driver.quit()
    # Removing carbon.png after uploading
    await e.delete() # Deleting msg

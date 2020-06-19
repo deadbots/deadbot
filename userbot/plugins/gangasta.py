be42e17cd106ba65b6b855b8e7b7b28e21397705
deadlyxd <66526325+deadlyxd@users.noreply.github.com>
2020-06-19 19:07:28 +0530

    Update gangasta.py

--- userbot/plugins/gangasta.py
+++ userbot/plugins/gangasta.py
@@ -5,21 +5,21 @@ import asyncio
 
 
 
-@borg.on(admin_cmd("gangasta ?(.*)"))
+@borg.on(admin_cmd("gg ?(.*)"))
 async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("Shreya")
+        await asyncio.sleep(0.3)
+        await event.edit("Ki")
+        await asyncio.sleep(0.2)
+        await event.edit("Support")
+        await asyncio.sleep(0.5)
+        await event.edit("Me")
+        await asyncio.sleep(0.2)
         await event.edit("Dead")
         await asyncio.sleep(0.3)
         await event.edit("Boy")
-        await asyncio.sleep(0.2)
-        await event.edit("Is")
-        await asyncio.sleep(0.5)
-        await event.edit("Biggest")
-        await asyncio.sleep(0.2)
-        await event.edit("Gangster")
         await asyncio.sleep(0.3)
-        await event.edit("😈😈😈")
+        await event.edit("Hai😈😈")
         await asyncio.sleep(0.3)
-        await event.edit("🔥🔥🔥")
-        await asyncio.sleep(0.3)
-        await event.edit("Dead Boy Is Biggest Gangster 😈😈😈 🔥🔥🔥")
+        await event.edit("Shreya Ki Support Me Dead Boy Hai😈😈")
